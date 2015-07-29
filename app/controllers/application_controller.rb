7d4507b8ba0a21bbb50912e05ba77e878bdf5254
ahmedhossammontasser <a.hossam.2010@gmail.com>
2015-07-29 20:01:04 +0200

    Finish sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   layout "add"
   protect_from_forgery
-
+  include SessionsHelper
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 DemoAppSample::Application.routes.draw do
   get "users/new"
 resources :users
-root to: 'static_pages#home'
+resources :sessions, only: [:new, :create, :destroy]
 
+root to: 'static_pages#home'
 match '/signup', to: 'users#new'
 
 match '/help', to: 'static_pages#help'
 match '/about', to: 'static_pages#about'
 match '/contact', to: 'static_pages#contact'
+
+match '/signup', to: 'users#new'
+match '/signin', to: 'sessions#new'
+match '/signout', to: 'sessions#destroy', via: :delete
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
