58daefac625150618dd48a8b4eebd6a938836eae
ahmedhossammontasser <a.hossam.2010@gmail.com>
2015-07-30 18:37:01 +0200

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,8 +21,10 @@ describe User do
 	it { should respond_to(:password_digest) }
 	it { should respond_to(:password_confirmation) }
 	it { should respond_to(:remember_token) }
+	it { should respond_to(:admin) }
 	it { should respond_to(:authenticate) }
 	it { should be_valid }
+	it { should_not be_admin }
 	it { should respond_to(:authenticate) }
 	describe "remember token" do
 		before { @user.save }
@@ -40,7 +42,13 @@ describe User do
 	before { @user.name = "a" * 51 }
 	it { should_not be_valid }
 	end
-
+	describe "with admin attribute set to 'true'" do
+		before do
+			@user.save!
+			@user.toggle!(:admin)
+		end
+		it { should be_admin }
+	end
 	describe "when email format is valid" do
 		it "should be valid" do
 		addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
